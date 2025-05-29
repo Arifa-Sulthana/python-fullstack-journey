fa20ad33d57733e3f8161681a13f49397d34b120
Arifa-Sulthana <63820176+Arifa-Sulthana@users.noreply.github.com>
2025-05-30 00:35:08 +0530

    Create dicts.py

---
+++ 03_Data_Structures/dicts.py
@@ -0,0 +1,9 @@
+profile = {
+    "name": "Zara",
+    "skills": ["Python", "ML"],
+    "location": "Bangalore"
+}
+profile["location"]="Remote"
+profile["experience"]=3
+profile["skills"].append("Data Analysis")
+print(profile)
